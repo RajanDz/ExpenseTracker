9b6e5702984c218ca7c0290a1e9bbcfb5c002d76
RajanDz <dzaferadzovicrajan@gmail.com>
2026-05-10 19:14:31 +0200

    fixed budgetDto response for easier frontend using

--- src/main/java/com/expenseTracker/expensetracker/controller/BudgetController.java
+++ src/main/java/com/expenseTracker/expensetracker/controller/BudgetController.java
@@ -49,14 +49,14 @@ public class BudgetController {
     public ResponseEntity<BudgetResponseDto> getBudget(@PathVariable(name = "id") long id, Authentication authentication){
         User user = userService.getLoggedUser(authentication);
         BudgetList budgetList = budgetService.getBudget(id,user);
-        return ResponseEntity.ok().body(new BudgetResponseDto(budgetList.getName(),budgetList.getBudget(),budgetList.getRemainingBudget(),budgetList.getStartDate(),budgetList.getEndDate(),String.valueOf(budgetList.getType())));
+        return ResponseEntity.ok().body(new BudgetResponseDto(budgetList.getId(),budgetList.getName(),budgetList.getBudget(),budgetList.getRemainingBudget(),budgetList.getStartDate(),budgetList.getEndDate(),String.valueOf(budgetList.getType())));
     }
 
     @GetMapping("/getPrimaryBudget")
     public ResponseEntity<BudgetResponseDto> getDefaultBudget(Authentication authentication){
         User user = userService.getLoggedUser(authentication);
         BudgetList budgetList = budgetService.getDefaultBudget(user);
-        return ResponseEntity.ok().body(new BudgetResponseDto(budgetList.getName(),budgetList.getBudget(),budgetList.getRemainingBudget(),budgetList.getStartDate(),budgetList.getEndDate(),String.valueOf(budgetList.getType())));
+        return ResponseEntity.ok().body(new BudgetResponseDto(budgetList.getId(),budgetList.getName(),budgetList.getBudget(),budgetList.getRemainingBudget(),budgetList.getStartDate(),budgetList.getEndDate(),String.valueOf(budgetList.getType())));
     }
     @GetMapping("/getBudgetExpenses/{id}")
     public ResponseEntity<BudgetDetailsResponse> getBudgetExpenses(@PathVariable(name = "id") Long id, Authentication authentication){
