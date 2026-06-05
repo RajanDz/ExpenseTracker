997d88308c7d238527d7f48da8aa6d27d32bc349
RajanDz <dzaferadzovicrajan@gmail.com>
2026-06-05 10:54:19 +0200

    add: categories response for frontend

--- src/main/java/com/expenseTracker/expensetracker/repository/ExpenseRepository.java
+++ src/main/java/com/expenseTracker/expensetracker/repository/ExpenseRepository.java
@@ -1,14 +1,17 @@
 package com.expenseTracker.expensetracker.repository;
 
+import com.expenseTracker.expensetracker.model.Category;
 import com.expenseTracker.expensetracker.model.Expense;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ExpenseRepository extends JpaRepository<Expense,Long> {
     Optional<Expense> findByIdAndBudgetId(long expenseId, long budgetId);
 
     Page<Expense> findByBudgetId(long budgetId, Pageable pageable);
+
 }
