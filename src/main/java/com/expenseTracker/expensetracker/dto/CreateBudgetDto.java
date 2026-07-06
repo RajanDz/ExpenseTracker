acb0426be978d0b3d0512091b93672a87bcf4940
RajanDz <dzaferadzovicrajan@gmail.com>
2026-07-06 10:45:49 +0200

    implement deactivating current active budget while creating a new one

--- src/main/java/com/expenseTracker/expensetracker/dto/CreateBudgetDto.java
+++ src/main/java/com/expenseTracker/expensetracker/dto/CreateBudgetDto.java
@@ -3,12 +3,14 @@ package com.expenseTracker.expensetracker.dto;
 import com.expenseTracker.expensetracker.model.BudgetTypes;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Getter
+@AllArgsConstructor
 public class CreateBudgetDto {
 
     @NotNull
--- src/main/java/com/expenseTracker/expensetracker/model/Budget.java
+++ src/main/java/com/expenseTracker/expensetracker/model/Budget.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Table(name = "budget_list")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Budget {
 
     public Budget(String name, BigDecimal budget, LocalDate startDate, LocalDate endDate, BudgetTypes type , boolean active, User user) {
@@ -88,7 +89,9 @@ public class Budget {
         expense.editAmount(newAmount);
 
     }
-
+    public void deactivate(){
+        this.active = false;
+    }
     public void updateBudgetAfterExpenseDelete(Expense expense){
         this.remainingBudget = this.remainingBudget.add(expense.getAmount());
     }
